36ef604c32823dc803006d24e13a5f45681f2b5b
jungtaeseop <epqlfepzk@naver.com>
2023-11-14 15:45:04 +0900

    test: test 한번에 여러개할 수 있도록 케이스 추가

--- src/test/java/christmas/domain/DiscountFactoryTest.java
+++ src/test/java/christmas/domain/DiscountFactoryTest.java
@@ -7,139 +7,110 @@ import java.util.List;
 import java.util.stream.Collectors;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class DiscountFactoryTest {
 
-    @DisplayName("할인 목록 생성 체크")
-    @Test
-    void 할인_목록_생성_체크() {
-        //given
-        Integer date = 26;
-        String menu = "초코케이크-2,아이스크림-1,제로콜라-1";
-        DiscountFactory discountFactory = new DiscountFactory(date, new Order(menu));
-
-        List<Integer> expectedDiscountAmounts = Arrays.asList(-6069);
-
-        //when
-        List<Integer> actualDiscountAmounts = discountFactory.getDiscounts()
-                .stream()
-                .map(item -> item.getDiscountAmount())
-                .collect(Collectors.toList());
-
-        //then
-        assertThat(actualDiscountAmounts).isEqualTo(expectedDiscountAmounts);
-    }
-
     @DisplayName("총 할인 금액 합산 체크")
-    @Test
-    void 총_할인_금액_합산_체크() {
+    @ParameterizedTest
+    @CsvSource(value = {"티본스테이크-2,바비큐립-1,초코케이크-2,아이스크림-1,레드와인-1:2:-32169", "초코케이크-1,제로콜라-2:29:0", "초코케이크-3:31:-7069", "초코케이크-5:24:-14415"}, delimiter = ':')
+    void 총_할인_금액_합산_체크(String menu, Integer date, Integer expected) {
         //given
-        Integer date = 2;
-        String menu = "티본스테이크-2,바비큐립-1,초코케이크-2,아이스크림-1,레드와인-1";
         DiscountFactory discountFactory = new DiscountFactory(date, new Order(menu));
 
         //when //then
-        assertThat(discountFactory.totalDiscountAmount()).isEqualTo(-32169);
+        assertThat(discountFactory.totalDiscountAmount()).isEqualTo(expected);
     }
 
     @DisplayName("이벤트 배지 체크")
-    @Test
-    void 이벤트_배지_체크() {
+    @ParameterizedTest
+    @CsvSource(value = {"티본스테이크-2,바비큐립-1,초코케이크-2,아이스크림-1,레드와인-1:2:산타", "초코케이크-1,제로콜라-2:15:없음", "초코케이크-3:31:별", "초코케이크-5:24:트리"}, delimiter = ':')
+    void 이벤트_배지_체크(String menu, Integer date, String expected) {
         //given
-        Integer date = 2;
-        String menu = "티본스테이크-2,바비큐립-1,초코케이크-2,아이스크림-1,레드와인-1";
         DiscountFactory discountFactory = new DiscountFactory(date, new Order(menu));
 
         //when //then
-        assertThat(discountFactory.eventBadge()).isEqualTo("산타");
+        assertThat(discountFactory.eventBadge()).isEqualTo(expected);
     }
 
     @DisplayName("크리스마스_디데이_할인")
-    @Test
-    void 크리스마스_디데이_할인() {
+    @ParameterizedTest
+    @CsvSource(value = {"초코케이크-2,아이스크림-1,레드와인-1:25:-3400", "티본스테이크-2,크리스마스파스타-2:1:-1000", "초코케이크-2:31:0"}, delimiter = ':')
+    void 크리스마스_디데이_할인(String menu, Integer date, Integer expected) {
         //given
-        Integer date = 25;
-        String menu = "티본스테이크-2,바비큐립-1,초코케이크-2,아이스크림-1,레드와인-1";
         DiscountFactory discountFactory = new DiscountFactory(date, new Order(menu));
-        Integer expectedDiscountAmounts = -3400;
 
         //when
         Integer ddayDiscount = discountFactory.getDiscounts().stream()
                 .filter(item -> "크리스마스 디데이 할인".equals(item.getEventName()))
                 .map(item -> item.getDiscountAmount())
                 .findFirst()
-                .get();
+                .orElse(0);
 
         //then
-        assertThat(ddayDiscount).isEqualTo(expectedDiscountAmounts);
+        assertThat(ddayDiscount).isEqualTo(expected);
     }
 
     @DisplayName("평일 할인")
-    @Test
-    void 평일_할인() {
+    @ParameterizedTest
+    @CsvSource(value = {"티본스테이크-1,바비큐립-1,초코케이크-2,아이스크림-1,레드와인-1:3:-6069", "티본스테이크-2,크리스마스파스타-2:25:0", "초코케이크-2:8:0"}, delimiter = ':')
+    void 평일_할인(String menu, Integer date, Integer expected) {
         //given
-        Integer date = 3;
-        String menu = "티본스테이크-1,바비큐립-1,초코케이크-2,아이스크림-1,레드와인-1";
         DiscountFactory discountFactory = new DiscountFactory(date, new Order(menu));
-        Integer expectedDiscountAmounts = -6069;
 
         //when
         int weekdayDiscount = discountFactory.getDiscounts().stream()
                 .filter(item -> "평일 할인".equals(item.getEventName()))
                 .map(item -> item.getDiscountAmount())
                 .findFirst()
-                .get();
+                .orElse(0);
 
         //then
-        assertThat(weekdayDiscount).isEqualTo(expectedDiscountAmounts);
+        assertThat(weekdayDiscount).isEqualTo(expected);
     }
 
     @DisplayName("주말_할인")
-    @Test
-    void 주말_할인() {
+    @ParameterizedTest
+    @CsvSource(value = {"티본스테이크-1,바비큐립-1,초코케이크-2,제로콜라-1:1:-4046", "티본스테이크-2,크리스마스파스타-2:23:-8092", "초코케이크-1:8:0"}, delimiter = ':')
+    void 주말_할인(String menu, Integer date, Integer expected) {
         //given
-        Integer date = 1;
-        String menu = "티본스테이크-1,바비큐립-1,초코케이크-2,제로콜라-1";
         DiscountFactory discountFactory = new DiscountFactory(date, new Order(menu));
-        Integer expectedDiscountAmounts = -4046;
 
         //when
         int weekendDiscount = discountFactory.getDiscounts().stream()
                 .filter(item -> "주말 할인".equals(item.getEventName()))
                 .map(item -> item.getDiscountAmount())
                 .findFirst()
-                .get();
+                .orElse(0);
 
         //then
-        assertThat(weekendDiscount).isEqualTo(expectedDiscountAmounts);
+        assertThat(weekendDiscount).isEqualTo(expected);
     }
 
     @DisplayName("특별_할인")
-    @Test
-    void 특별_할인() {
+    @ParameterizedTest
+    @CsvSource(value = {"초코케이크-2:26:0", "티본스테이크-2:25:-1000", "바비큐립-1:31:-1000"}, delimiter = ':')
+    void 특별_할인(String menu, Integer date, Integer expected) {
         //given
-        Integer date = 25;
-        String menu = "티본스테이크-1,바비큐립-1,초코케이크-2,제로콜라-1";
         DiscountFactory discountFactory = new DiscountFactory(date, new Order(menu));
-        Integer expectedDiscountAmounts = -1000;
 
         //when
         int specialDiscount = discountFactory.getDiscounts().stream()
                 .filter(item -> "특별 할인".equals(item.getEventName()))
                 .map(item -> item.getDiscountAmount())
                 .findFirst()
-                .get();
+                .orElse(0);
 
         //then
-        assertThat(specialDiscount).isEqualTo(expectedDiscountAmounts);
+        assertThat(specialDiscount).isEqualTo(expected);
     }
 
     @DisplayName("증정 이벤트 할인")
-    @Test
-    void 증정_이벤트_할인() {
+    @ParameterizedTest
+    @CsvSource(value = {"초코케이크-2:25:0", "티본스테이크-2,바비큐립-1,초코케이크-2,제로콜라-1:25:-25000", "티본스테이크-19,바비큐립-1:25:-25000"}, delimiter = ':')
+    void 증정_이벤트_할인(String menu, Integer date, Integer expected) {
         //given
-        Integer date = 25;
-        String menu = "티본스테이크-1,바비큐립-1,초코케이크-2,제로콜라-2";
         DiscountFactory discountFactory = new DiscountFactory(date, new Order(menu));
 
         //when
@@ -147,9 +118,9 @@ class DiscountFactoryTest {
                 .filter(item -> "증정 이벤트".equals(item.getEventName()))
                 .map(item -> item.getDiscountAmount())
                 .findFirst()
-                .get();
+                .orElse(0);
 
         //then
-        assertThat(giftEventDiscount).isEqualTo(-25000);
+        assertThat(giftEventDiscount).isEqualTo(expected);
     }
 }
